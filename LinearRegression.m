4e0b2a1c86af214a9c49593655489166bfccafbc
Springo <kevinmxia@gmail.com>
2016-09-17 12:05:51 -0400

    testing files

--- LinearRegression.m
+++ LinearRegression.m
@@ -3,22 +3,35 @@ close all;
 clc
 
 %% Load Data
+
 fprintf('Loading data ...\n');
 X = load('train_features_x.txt');
 y = load('train_labels_y.txt');
+Xpred = load('input_features_x.txt');
+[m numfeat] = size(X);
+[mpred numpredfeat] = size(Xpred);
 
 %% Preprocess Data
-X = [ones(y,1) X]; % add intercept
-[m numfeat] = size(X);
+
+X = [ones(m,1) X]; % add intercept to training set
+Xpred = [ones(mpred,1) Xpred]; % add intercept to prediction set
+numfeat = numfeat + 1;
 
 %% Training
 
 fprintf('Running gradient descent ...\n');
 
-alpha = 0.01;
+alpha = 1;
 num_iters = 400;
 
 theta = zeros(numfeat, 1);
 [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters);
 
 %% Predicting
+
+theta
+
+f = fopen('output_labels_y.txt', 'w');
+results = Xpred * theta;
+fprintf(f,'%f\n',results);
+fclose(f);
